7899ba88b30c92e76037595ea8ddd6df753f0712
Bobby Elmore <bobbye23899@gmail.com>
2026-06-12 23:56:26 -0700

    Status Box CSS

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,7 +5,16 @@ import "../styles/Header.css";
 const Header = () => {
     return (
         <header className="header component-secondary">
-            <div className="status-box">Not connected.</div>
+            <div className="status-box">
+                <img
+                    class="status-icon"
+                    src="/img/no-wifi.png"
+                    alt="Status Icon"
+                />
+                <p id="status-text" className="status-text">
+                    Not connected.
+                </p>
+            </div>
             <div className="app-title">Discord Bridge</div>
             <div className="socials">
                 <SocialButton
